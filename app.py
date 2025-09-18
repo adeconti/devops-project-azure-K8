1473487369c778244af10c8d493a8f5ae8726f52
adeconti <116734999+adeconti@users.noreply.github.com>
2025-09-17 22:48:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Te amo muito Alice!</h1>"
+    return "<h1>Olá, DevOps na Accurate!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
